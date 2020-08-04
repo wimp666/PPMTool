2a92c74747d6ddcd8ff58e7e95b6cddf300b60f9
wimp666 <qwy19960215@gmail.com>
2020-08-03 19:21:49 -0700

    validation

---
+++ PPMTools/src/main/java/io/ppmtool/services/ProjectService.java
@@ -0,0 +1,18 @@
+package io.ppmtool.services;
+
+import io.ppmtool.domain.Project;
+import io.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+
+        return projectRepository.save(project);
+    }
+}
