88912ec19f31c2185f8d3aeb8757659eb4bd824a
wimp666 <qwy19960215@gmail.com>
2020-08-11 00:11:43 -0700

    reduxAddProject

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -6,17 +6,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import { Component } from "react";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connetc, connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -26,7 +29,7 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date,
     };
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -102,5 +105,8 @@ class AddProject extends Component {
     );
   }
 }
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired,
+};
 
-export default AddProject;
+export default connect(null, { createProject })(AddProject);
