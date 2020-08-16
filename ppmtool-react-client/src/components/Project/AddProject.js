a23ecd703d07aada062f3b7f827d3bede75ed098
wimp666 <qwy19960215@gmail.com>
2020-08-15 18:16:13 -0700

    errorReducer

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -12,9 +12,17 @@ class AddProject extends Component {
       description: "",
       start_date: "",
       end_date: "",
+      errors: {},
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  //life cycle hooks
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onChange(e) {
@@ -33,6 +41,8 @@ class AddProject extends Component {
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div>
         <div className="project">
@@ -51,6 +61,7 @@ class AddProject extends Component {
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectName}</p>
                   </div>
                   <div className="form-group">
                     <input
@@ -62,6 +73,7 @@ class AddProject extends Component {
                       onChange={this.onChange}
                     />
                   </div>
+                  <p>{errors.projectIdentifier}</p>
                   <div className="form-group">
                     <textarea
                       className="form-control form-control-lg"
@@ -71,6 +83,7 @@ class AddProject extends Component {
                       onChange={this.onChange}
                     ></textarea>
                   </div>
+                  <p>{errors.description}</p>
                   <h6>Start Date</h6>
                   <div className="form-group">
                     <input
@@ -107,6 +120,11 @@ class AddProject extends Component {
 }
 AddProject.propTypes = {
   createProject: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
-export default connect(null, { createProject })(AddProject);
+const mapStateToProps = (state) => ({
+  errors: state.errors,
+});
+
+export default connect(mapStateToProps, { createProject })(AddProject);
