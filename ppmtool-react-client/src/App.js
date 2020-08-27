f826c2991a3de3c061fb2e426a9774aef1dd06c6
wimp666 <qwy19960215@gmail.com>
2020-08-26 18:44:09 -0700

    update project

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,7 @@ import AddProject from "./components/Project/AddProject";
 import { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import UpdateProject from "./components/Project/UpdateProject";
 
 class App extends Component {
   render() {
@@ -18,6 +19,7 @@ class App extends Component {
             <Header />
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
           </div>
         </Router>
       </Provider>
